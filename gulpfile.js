425a2315e11323a0193f6ae138ab4b855b147345
Jovan Milovanovic <jmilovanovic@gogoair.com>
2018-09-20 14:02:59 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'),
+		sass = require ('gulp-sass'),
+		notify = require('gulp-notify'),
+		filter = require('gulp-filter'),
+		autoprefixer = require('gulp-autoprefixer'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		imagemin = require('gulp-imagemin');
+		
+var config = {
+	stylesPath: 'assets/styles',
+	jsPath: 'assets/scripts',
+	outputDir: 'public/dist'
+}
+
+
+
+gulp.task('icons', function() { 
+	return gulp.src('./node_modules/font-awesome/fonts/**.*') 
+		.pipe(gulp.dest(config.outputDir + '/fonts')); 
+});
+
+gulp.task('images', function() { 
+	return gulp.src(config.imagesPath + '/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest(config.outputDir + '/images'))
+});
+
+gulp.task('css', function() {
+	return gulp.src(config.stylesPath + '/main.scss')
+		.pipe(sass({
+				outputStyle: 'compressed',
+				includePaths: [
+					config.stylesPath,
+					'./node_modules/font-awesome/scss'
+				]
+			}).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(config.outputDir + '/css'));
+});
+
+
+gulp.task('jquery', function(){
+	return gulp.src('./node_modules/jquery/dist/jquery.min.js') 
+		.pipe(gulp.dest(config.outputDir + '/js')); 
+});
+
+gulp.task('js', function() {
+	return gulp.src(config.jsPath+'/*')
+		.pipe(filter('**/*.js'))
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.outputDir + '/js'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch([config.stylesPath + '**/*.scss', config.stylesPath + '**/*.scss', config.stylesPath + '**/*.css'], ['css']);
+	gulp.watch([config.jsPath + '**/*.js'], ['js']);
+	gulp.watch([config.imagesPath + '/**/*'], ['images']);
+});
+
+gulp.task('default', ['icons', 'css', 'jquery', 'js']);
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Bootstrap 4 project boilerplate
+
+A boilerplate for Bootstrap 4, FontAwesome and jQuery using Yarn and Gulp
+
+
+## Installation
+
+1. Clone this repo.
+2. cd into directory and run `yarn`
+3. Run `gulp` to compile Sass and JS
+4. Run `gulp watch` to monitor your JS and SASS
